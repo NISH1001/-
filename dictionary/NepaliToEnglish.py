caf2046b1025a7895617be6edc10e6627278e8e2
Bibekpandey <spirit_bibek@yahoo.com>
2015-07-30 09:17:24 +0545

    modified nepalitoenglish.py

--- dictionary/NepaliToEnglish.py
+++ dictionary/NepaliToEnglish.py
@@ -2,6 +2,8 @@
 
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
 '''
@@ -15,82 +17,48 @@ class ScraperError(Exception):
         print('ScraperError: '+''.join(self.args))
 
 
-''' 
-    class MeaningScraper:
-        We pass in the nepali word and it outputs
-        corresponding meaning if found.
 
-    METHODS:
-        Constructor             : takes the url of site, and a True/False flag
-                                    to indicate whether it requires preeti font or not
-        SetPreetiConverterUrl   : we feed in the url whose page converts unicode to
-                                    preeti.
-        GetPreeti               : this visits the webpage and returns the preeti equivalent
-                                    of the unicode fed
-        GetEngWord              : this receives a unicode and calls GetPreeti() to get preeti
-                                    and then calls ScrapEnglishWord() to get the eng equivalent
-        ScrapEnglishWord        : does the real scraping of nepali word(Preeti) fed to it
+nepali_words = ['म', 'मेरो'] # nepali words in unicode
+nep_eng = {} # stored as -> {nep_word: [eng_meanings]}
 
-'''
+# first start selenium webdriver
+driver = webdriver.Firefox()
+driver.get("http://nirazz.blogspot.com/2014/07/roman-and-unicode-to-preeti-nepali-font.html") # our unicode to preeti site
 
-class NepaliToEnglish(object):
-    def __init__(self, main_url, preeti_required=False):
-        # the url where we seek the meaning
-        self.url = main_url     
-        # if the site requires preeti font
-        self.preeti_required = preeti_required
-        self.preeti_converter_url = None
+def get_preeti(unicode_txt):
+    global driver
+    unicode_box = driver.find_element_by_id("unicode_text")
+    unicode_box.clear()
+    unicode_box.send_keys(unicode_txt)
+    convert_button = driver.find_element_by_xpath("//input[@name='unicode']")
+    convert_button.send_keys(Keys.RETURN)
+    preeti_box = driver.find_element_by_id("legacy_text")
+    return preeti_box.get_attribute('value')
 
-    def SetPreetiConverterUrl(self, url):
-        self.preeti_converter_url = url
+def find_meanings(raw):
+    splitted = raw.split(',')
+    return [x.strip() for x in splitted if x != '']
 
-    def GetEngWord(self, nep_unicode):
-        if self.preeti_required:
-            nep_word = self.GetPreeti(nep_unicode)
-        else:
-            nep_word = nep_unicode
+for x in nepali_words:
+    preeti = get_preeti(x)
+    print(preeti)
+    result = requests.post("http://dictionary.com.np/nepali.php",
+                {'search2nep':preeti,
+                'submit1':'Search'}
+    )
+    result_text = result.text
 
-        meaning = self.ScrapEnglishWord(nep_word)
-        return meaning
+    soup = BeautifulSoup(result_text, "html.parser")
+    result_elem = soup.findAll(attrs={'class':'font11'})
 
-    # not actual translation, only preeti eqv of unicode is returned
-    def GetPreeti(self, unicode_str):
-        if self.preeti_converter_url is None:
-            raise ScraperError("Preeti Converter Url not set!!")
+    if len(result_elem) != 4:
+        continue
+    # Extract the meaning, if not present, continue
+    raw = result_elem[0].text
+    if raw == None or raw == '':
+        continue
+    # in case multiple meanings are present
+    nep_eng[x] = find_meanings(raw)
 
-        post_data = {
-            'userInput' : unicode_str
-        }
-        response = requests.post(self.preeti_converter_url, post_data)
-        response_text = response.text
-
-        soup = BeautifulSoup(response_text, "html.parser")
-        # get the preeti equivalent to feed
-        try:
-            result_objects = soup.find_all("textarea")
-            for x in result_objects:
-                if x.attrs['name'] == "output":
-                    return x.text
-            raise Exception
-        except Exception:
-            raise ScraperError('Problem with finding the Preeti equivalent')
-
-    # actual translation of nepali word to english
-    def ScrapEnglishWord(self, nepali_word):
-        post_data = {
-            'search2nep' : nepali_word
-        }
-        response = requests.post(self.url, post_data)
-        resonse_text = response.text
-
-        soup = BeautifulSoup(resonse_text, "html.parser")
-        try:
-            result_objects = soup.find_all('font')
-            # iterate it, the first with class font11 is result
-            for x in result_objects:
-                if x.has_attr('class') and 'font11' in x.attrs['class']:
-                    return x.text
-            raise ''
-        except Exception:
-            raise ScraperError('Problem finding the english equivalent of the preeti font Entered')
-
+print(nep_eng)
+driver.close()
