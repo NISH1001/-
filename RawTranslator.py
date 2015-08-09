bf147696282f3a1bed558bd40b8c2b84fc7501c7
Bibekpandey <spirit_bibek@yahoo.com>
2015-08-09 15:05:00 +0545

    wrote abstract function to parse biphrases, testing needed

--- RawTranslator.py
+++ RawTranslator.py
@@ -29,6 +29,15 @@ class RawTranslator(object):
 
     def __init__(self, db):
         self.dict_handler = DictionaryDBHandler(db)
+        self.tense_structures = [] # initialize with the tenses
+        self.simple_present = []
+        self.simple_past = []
+        self.simple_future = []
+        self.simple_tenses = [self.simple_present, self.simple_past, self.simple_future]
+        self.continuous = []
+        self.perfect = []
+        self.perfect_continuous = []
+        self.non_simple_tenses = [self.continuous, self.perfect, self.perfect_continuous]
     
     def translate(self, nepali_text):
         try:
@@ -39,10 +48,12 @@ class RawTranslator(object):
             bigrams = [' '.join(words[x:x+2]) for x in range(len(words)-1)]
 
             # Check each ngram whether it is action or not 
+            # In the first phase, we check for actions involving biphrases
+            # After that, we check for single ones
 
             '''
             for i, item in enumerate(bigrams):
-                eng_phrase = get_english_eqv(item)
+                eng_phrase = self.get_action(item) # checks bigram
                 if eng_phrase is not None: # means phrase match found
                     re.sub(item, eng_phrase, nepali_text, 1)
             '''
@@ -53,11 +64,15 @@ class RawTranslator(object):
 
             for x in words:
                 if 1 or is_nepali(x): # **** 1 is just for debugging
-                    eng_words.append('^^'.join(list(
+                    eng_word = '^^'.join(list(
                             map(lambda x: x.lower(), self.dict_handler.get_english(x))
                             )
-                        )
-                    ) # CNF separated by ^^
+                        ) # CNF separated by ^^
+
+                    if eng_word=='': # which is the case when no meaning found in dict
+                        eng_word==get_action(x) # checks unigram
+
+                    eng_words.append(eng_word)
                 else:
                     eng_words.append(x.lower())
 
@@ -67,13 +82,49 @@ class RawTranslator(object):
             print('error occured')
             assert False
 
-    def get_eng_eqv(self, nepali_phrase):
+    def get_action(self, nepali_phrase): # nepali phrase is bigram/unigram for now
+        # Check if the bigram matches any form in our tenses    
+        for simple_tense in self.simple_tenses:
+            for structure in simple_tense:
+
+                # Check if the phrase's second part matches fully with the structure
+                # If so it is not only simple, check first part too
+                simple_result = re.search(' ('+structure+')', nepali_phrase)
+
+                if simple_result is not None:
+                    # Search for match in the first part of the phrase
+                    first_part = nepali_phrase.split()[0]
+                    # Now check in continuous, perfect and perfect continuous lists
+                    for non_simple in self.non_simple_tenses:
+                        # Here, full part won't match, so check partial
+                        for each in non_simple:
+                            non_simple_result = re.search('(\S)'+structure, first_part)
+                            
+                            if non_simple_result is None:
+                                raise Exception('either phrase is wrong, or internal error while parsing :', nepali_phrase)
+
+                            # Here, we have the verb root in non_simple_root, so extract verb
+                            verb = extract_verb(non_simple_result.group(1))
+
+                            return '' # here return the correct tense of the verb
+
+                # Else if the simple_result returned none, which means, we check only the partial part
+                else:
+                    simple_result = re.search(' (\S)'+structure, nepali_phrase)
+                    if simple_result is None:
+                        return None # The phrase is not an action
+                        raise Exception('either phrase is wrong, or internal error while parsing :', nepali_phrase)
+                    verb = extract_verb(simple_result.group(1))
+                    return '' # return the correct tense of the verb
+                        
+
         pass
+        
 
 
 def main():
     translator = RawTranslator("dictionary/dictionary.db")
-    print(translator.translate("घर जाउ"))
+    print(translator.translate("खाना राम्रो"))
 
 
 if __name__=='__main__':
